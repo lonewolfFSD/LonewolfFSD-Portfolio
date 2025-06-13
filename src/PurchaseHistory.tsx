99d9f5ea531cf34672b2abef556a64775f0f6cdb
Lonewolf FSD <lonewolffsd@gmail.com>
2025-06-13 22:00:14 +0530

    Update PurchaseHistory.tsx

--- src/PurchaseHistory.tsx
+++ src/PurchaseHistory.tsx
@@ -59,12 +59,12 @@ const PurchaseHistory: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-100 sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto bg-white p-6 sm:p-12 border border-black rounded-xl">
         <div className="flex justify-between items-center mb-8">
           <h2
             style={{ fontFamily: "Poppins" }}
-            className="text-3xl sm:text-4xl font-bold text-left"
+            className="text-3xl sm:text-4xl font-bold mt-6 md:mt-auto text-left"
           >
             Purchase History
           </h2>
